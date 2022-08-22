1a378cf96cbd53b6b6b747101c10a1f14034f7e3
Tanguy Billon <tanguy_billon@hotmail.fr>
2022-08-22 08:15:45 +0200

    add 22.08.22 cpp_module_00

---
+++ cpp_module_00/22.08.22/Warlock.hpp
@@ -0,0 +1,29 @@
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <iostream>
+#include <string>
+
+class Warlock
+{
+	public:
+		Warlock(std::string name, std::string title);
+		~Warlock();
+
+		std::string const &getName() const {return this->_name;}
+		std::string const &getTitle() const {return this->_title;}
+
+		void		setTitle(const std::string title) {this->_title = title;}
+
+		void		introduce() const;
+
+	private:
+		std::string	_name;
+		std::string	_title;
+
+		Warlock();
+		Warlock(const Warlock &other);
+		Warlock	&operator=(const Warlock &other);
+};
+
+#endif
